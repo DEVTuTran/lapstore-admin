97167aba659cde15bfc191f547e1a61c33c0958f
DEVTranVanTu <Dev.tutran@gmail.com>
2022-11-30 21:53:34 +0700

    change host serve

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'http://localhost:5555/api/',
+  api_url: 'http://34.124.138.15:5555/api/',
   province_api: 'https://provinces.open-api.vn/api/?depth=3',
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 export const environment = {
   production: false,
-  api_url: 'http://localhost:5555/api/',
+  api_url: 'http://34.124.138.15:5555/api/',
   province_api: 'https://provinces.open-api.vn/api/?depth=3',
   firebase: {
     apiKey: 'AIzaSyB-nck035aktj_6EUlqYIOhL7uuwBAWOx0',
